e42948e7a38ab7aee2bcd32e37957d95b1a5f272
Bzi_Han <rosebzi@foxmail.com>
2022-07-27 12:32:53 +0800

    Update response encryption

--- src/main/java/com/bzi/taskcloud/security/auth/JwtFilter.java
+++ src/main/java/com/bzi/taskcloud/security/auth/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.bzi.taskcloud.security.auth;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.bzi.taskcloud.common.lang.Result;
 import com.bzi.taskcloud.common.lang.UserState;
@@ -7,12 +8,15 @@ import com.bzi.taskcloud.common.lang.UserType;
 import com.bzi.taskcloud.common.utils.JwtUtil;
 import com.bzi.taskcloud.common.utils.LoggerUtil;
 import com.bzi.taskcloud.entity.User;
+import com.bzi.taskcloud.security.data.DataCrypto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.security.auth.login.AccountLockedException;
+import javax.security.auth.login.AccountNotFoundException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -30,48 +34,53 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // 检测当前请求是否存在JwtToken
-        if (StringUtils.isNotBlank(request.getHeader("Authorization"))) {
-            User user = null;
+        try {
+            // 检测当前请求是否存在JwtToken
+            if (StringUtils.isNotBlank(request.getHeader("Authorization"))) {
+                // 通过JwtToken获取用户信息
+                User user = JwtUtil.getUserFromToken(request.getHeader("Authorization"));
 
-            // 通过JwtToken获取用户信息
-            try {
-                user = JwtUtil.getUserFromToken(request.getHeader("Authorization"));
-            } catch (Exception e) {
-                LoggerUtil.failed(e.getMessage());
-            }
+                if (null != user) {
+                    // 检测用户是否已注销
+                    if (UserState.delete.ordinal() == user.getState())
+                        throw new AccountNotFoundException("不存在的账户。");
 
-            if (null != user) {
-                // 检测用户是否已注销
-                if (UserState.delete.ordinal() == user.getState()) {
-                    response.setCharacterEncoding("UTF-8");
-                    response.setStatus(400);
-                    response.getWriter().write(new ObjectMapper().writeValueAsString(
-                            Result.failed("不存在的账户。")
-                    ));
+                    // 检测用户是否已被冻结
+                    if (UserState.freeze.ordinal() == user.getState())
+                        throw new AccountLockedException("您的账户已被冻结，如有需要请联系管理员！");
 
-                    return;
+                    // 设置登陆的用户信息
+                    SecurityContextHolder.getContext().setAuthentication(
+                            new UsernamePasswordAuthenticationToken(
+                                    user,
+                                    null,
+                                    List.of(USER_TYPE_MAPPING.get(user.getType()))
+                            )
+                    );
                 }
-                // 检测用户是否已被冻结
-                if (UserState.freeze.ordinal() == user.getState()) {
-                    response.setCharacterEncoding("UTF-8");
-                    response.setStatus(400);
-                    response.getWriter().write(new ObjectMapper().writeValueAsString(
-                            Result.failed("您的账户已被冻结，如有需要请联系管理员！")
-                    ));
-
-                    return;
-                }
-
-                // 设置登陆的用户信息
-                SecurityContextHolder.getContext().setAuthentication(
-                        new UsernamePasswordAuthenticationToken(
-                                user,
-                                null,
-                                List.of(USER_TYPE_MAPPING.get(user.getType()))
-                        )
-                );
             }
+        } catch (Exception e) {
+            LoggerUtil.failed(e.getMessage());
+
+            Result result;
+            if (e instanceof TokenExpiredException)
+                result = Result.make(401, "您的登陆已过期，请重新登陆！", null);
+            else if (e instanceof AccountNotFoundException)
+                result = Result.make(400, e.getMessage(), null);
+            else if (e instanceof AccountLockedException)
+                result = Result.make(403, e.getMessage(), null);
+            else
+                result = Result.failed("系统异常，请联系管理员！");
+
+            var resultText = DataCrypto.encrypt(new ObjectMapper().writeValueAsString(
+                    result
+            ));
+
+            response.setCharacterEncoding("UTF-8");
+            response.setStatus(result.getCode());
+            response.getWriter().write(resultText);
+
+            return;
         }
 
         // 继续执行过滤器
